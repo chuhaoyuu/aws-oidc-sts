7ec3f493d52fc4ab708aaa8f8dd37a1578c9fd5e
Jacky Chu <chuhaoyuu@gmail.com>
2025-06-17 15:47:06 +0800

    feat: add create command for generating RSA key pairs and JWKS
    Introduce a new command to create resources such as RSA key pairs and JSON Web Key Sets (JWKS). This command allows users to generate cryptographic assets and save them to a specified target directory, enhancing the CLI tool's functionality for managing OIDC and STS resources in AWS.

---
+++ cmd/root.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var TargetDir string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "aws-oidc-sts",
+	Short: "A CLI tool for managing OIDC and STS resources in AWS",
+	Long: `aws-oidc-sts is a command-line tool designed to simplify the management 
+of OpenID Connect (OIDC) and Security Token Service (STS) resources in AWS. 
+It provides commands to generate cryptographic assets, configure identity providers, 
+and manage related resources.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Error getting current working directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	rootCmd.Root().CompletionOptions.DisableDefaultCmd = true
+	rootCmd.AddCommand(createCmd)
+	rootCmd.PersistentFlags().StringVarP(&TargetDir, "output-dir", "o", pwd, "Target directory for the generated files")
+}
